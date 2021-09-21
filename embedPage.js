b30d61f472f88bc98f937ef36d9a6f28c27ae1f2
Joel Johnson <jjoel1630@gmail.com>
2021-09-20 18:28:06 -0700

    changes

--- embedPage.js
+++ embedPage.js
@@ -1,3 +1,18 @@
-var div = document.createElement("div");
-document.body.appendChild(div);
-div.innerText = "test";
+if (document.readyState !== "complete") {
+	window.addEventListener("load", afterWindowLoaded);
+} else {
+	afterWindowLoaded();
+}
+
+function afterWindowLoaded() {
+	setTimeout(() => {
+		//Everything that needs to happen after the window is fully loaded.
+		//Everything that needs to happen after the DOM has initially loaded.
+		const form = document.createElement("div");
+		form.innerText = "test";
+		const containerDiv = document.querySelector(".ng-tns-c426-3 > #document");
+		// containerDiv.innerHTML = `${form}${containerDiv.innerHTML}`;
+		// containerDiv.innerHTML = `<h1>test</h1>`;
+		console.log(containerDiv);
+	}, 8000);
+}
