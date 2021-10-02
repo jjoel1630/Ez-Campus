c0d2cdb68b39680d822db27eea8a7b6641ec0180
Joel Johnson <jjoel1630@gmail.com>
2021-10-02 13:57:41 -0700

    added jquery

--- embedPage.js
+++ embedPage.js
@@ -6,13 +6,8 @@ if (document.readyState !== "complete") {
 
 function afterWindowLoaded() {
 	setTimeout(() => {
-		//Everything that needs to happen after the window is fully loaded.
-		//Everything that needs to happen after the DOM has initially loaded.
-		const form = document.createElement("div");
-		form.innerText = "test";
-		const containerDiv = document.querySelector(".ng-tns-c426-3 > #document");
-		// containerDiv.innerHTML = `${form}${containerDiv.innerHTML}`;
-		// containerDiv.innerHTML = `<h1>test</h1>`;
-		console.log(containerDiv);
+		let extensionID = "gekdfbljcjbnkhmmophbpbnapjpcicno";
+		let elementTOUpdate = $(".workspace").get();
+		console.log(elementTOUpdate);
 	}, 8000);
 }
