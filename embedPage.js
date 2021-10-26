e3d3fa0bdefd67a2b20faeab2041230188a24f74
Joel Johnson <jjoel1630@gmail.com>
2021-10-26 13:17:44 -0700

    idek

--- embedPage.js
+++ embedPage.js
@@ -5,12 +5,13 @@ if (document.readyState !== "complete") {
 }
 
 function afterWindowLoaded() {
+	// while
 	setTimeout(() => {
 		//Everything that needs to happen after the window is fully loaded.
 		//Everything that needs to happen after the DOM has initially loaded.
 		const form = document.createElement("div");
 		form.innerText = "test";
-		const containerDiv = document.querySelector(".ng-tns-c426-3 > #document");
+		const containerDiv = document.querySelector("ic-work");
 		// containerDiv.innerHTML = `${form}${containerDiv.innerHTML}`;
 		// containerDiv.innerHTML = `<h1>test</h1>`;
 		console.log(containerDiv);
