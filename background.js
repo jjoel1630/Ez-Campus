76ac9fe9c887d237e0c4b6f7c12932b966e12a02
Joel Johnson <jjoel1630@gmail.com>
2021-10-30 10:20:20 -0700

    changes

---
+++ background.js
@@ -0,0 +1,10 @@
+chrome.webRequest.onCompleted.addListener(
+	(details) => {
+		const parsedUrl = new URL(details.url);
+		console.log(parsedUrl);
+		if (currentUrl && currentUrl.indexOf(parsedUrl.pathname) > -1 && tabId) {
+			chrome.tabs.sendMessage(tabId, { type: "page-rendered" });
+		}
+	},
+	{ urls: ["*://*.org/"] }
+);
--- embedPage.js
+++ embedPage.js
@@ -1,13 +1,25 @@
-if (document.readyState !== "complete") {
-	window.addEventListener("load", afterWindowLoaded);
-} else {
-	afterWindowLoaded();
+// if (document.readyState !== "complete") {
+// 	window.addEventListener("load", afterWindowLoaded);
+// } else {
+// 	afterWindowLoaded();
+// }
+
+// function afterWindowLoaded() {
+// 	setTimeout(() => {
+// 		let extensionID = "gekdfbljcjbnkhmmophbpbnapjpcicno";
+// 		let elementTOUpdate = $(".workspace").get();
+// 		console.log(elementTOUpdate);
+// 	}, 8000);
+// }
+
+function afterwindowLoaded() {
+	let elementTOUpdate = $(".workspace").get();
+	console.log(elementTOUpdate);
 }
 
-function afterWindowLoaded() {
-	setTimeout(() => {
-		let extensionID = "gekdfbljcjbnkhmmophbpbnapjpcicno";
-		let elementTOUpdate = $(".workspace").get();
-		console.log(elementTOUpdate);
-	}, 8000);
-}
+chrome.runtime.onMessage.addListener((request) => {
+	if (request && request.type === "page-rendered") {
+		// call method which gets fired as if new page is opened
+		afterwindowLoaded();
+	}
+});
