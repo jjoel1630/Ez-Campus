62445b7ce1c01534ae723844d19d8c572fc2c8f7
Joel Johnson <jjoel1630@gmail.com>
2021-11-22 17:49:01 -0800

    cleaned up content-script

--- javascript/content-script-neat.js
+++ javascript/content-script-neat.js
@@ -1,59 +1,127 @@
-// setTimeout(() => {
-// 	const iframeContainer = document.getElementById("main-workspace");
+function css(element, style) {
+	for (const property in style) element.style[property] = style[property];
+}
 
-// 	if (
-// 		iframeContainer.contentWindow.document
-// 			.querySelector(".k-button")
-// 			?.getAttribute("aria-pressed")
-// 	) {
-// 		const mDiv = document.createElement("div");
-// 		const header = document.createElement("h2");
-// 		header.textContent = "Please choose a quarter for this extension to work!";
-// 		mDiv.append(header);
-// 		css(mDiv, {
-// 			width: "100%",
-// 			height: "4rem",
-// 			display: "flex",
-// 			"justify-content": "center",
-// 			"align-items": "center",
-// 		});
-// 		iframeContainer.contentWindow.document.querySelector(".workspace-content").prepend(mDiv);
+const calcTotalGrade = (localStorageObject) => {
+	let totalGrade = 0;
+	let totalWeight = 0;
+	localStorageObject.forEach(({ weight, percent }, idx) => {
+		totalGrade += parseInt(weight) * parseFloat(parseFloat(percent.slice(1, -2)));
+		totalWeight += parseInt(weight);
+	});
+	totalGrade = totalGrade / totalWeight;
+	return totalGrade;
+};
 
-// 		const len = iframeContainer.contentWindow.document.querySelectorAll(
-// 			".pill-button-group.k-button-group > button"
-// 		).length;
+const calcGradeBasedOnLocalObject = (title, category, firstTotalValue, secondTotalValue) => {
+	let newLocalStorage = [];
 
-// 		iframeContainer.contentWindow.document
-// 			.querySelectorAll(".pill-button-group.k-button-group > button")
-// 			[len - 1].addEventListener("click", (e) => {
-// 				// console.log("in e listener");
-// 				mDiv.style.display = "none";
-// 			});
+	JSON.parse(localStorage.getItem("course_info"))[`${title}`].forEach((obj, idx) => {
+		let scoreObj; // the score from the object
+		let weight; // the weight of the category
+		newLocalStorage.push({});
+		// IF THE CATEGORY IS THE OBJECT NAME --> THE ASSIGNMENT IS ADDED TO THAT CATEGORY
+		if (obj.name === category.textContent) {
+			// SPLIT THE SCORE PROPERTY FROM THE MAIN OBJECT
+			scoreObj = obj.score;
+			scoreObj = scoreObj.split("/");
 
-// 		return;
-// 	}
-// });
+			// GET THE WEIGHT PROPERTY FROM THE MAIN OBJECT
+			weight = parseInt(obj.weight);
 
-// let button;
-// setTimeout(() => {
-// 	const iframeContainer = document.getElementById("main-workspace");
-// 	const t = iframeContainer.contentWindow.document.querySelectorAll(
-// 		".pill-button-group.k-button-group > button"
-// 	);
-// 	button = t[t.length - 1];
-// 	console.log(button);
-// }, 10000);
+			// totalWeight += weight;
 
-setTimeout(() => {
+			// GET TOTAL POSSIBLE POINTS AND TOTAL POINTS EARNED TO DIVIDE THEM
+			let totalPtsPossible = parseInt(scoreObj[1]) + parseInt(secondTotalValue);
+			let totalPtsEarned = parseInt(scoreObj[0]) + parseInt(firstTotalValue);
+
+			newLocalStorage[idx].score = `${totalPtsEarned}/${totalPtsPossible}`;
+
+			// CALCULATE THE PERCENT OF THE WEIGHTAGE
+			let weightGrade = weight * (totalPtsEarned / totalPtsPossible / 100);
+			newLocalStorage[idx].percent = `(${((totalPtsEarned / totalPtsPossible) * 100).toFixed(
+				2
+			)}%)`;
+
+			newLocalStorage[idx].name = obj.name;
+			newLocalStorage[idx].weight = obj.weight;
+
+			// ADD THE WEIGHT GRADE TO THE TOTAL GRADE
+			// totalGrade += weightGrade;
+		} else {
+			// IF THE CATEGORY THAT IS ADDED IS NOT THE OBJECT
+			// GET PERCENT OF THE OBJECT
+			// let objPercent = obj.percent.slice(1);
+			// objPercent = objPercent.slice(0, -2);
+			// objPercent = parseFloat(objPercent);
+
+			// WEIGHT OF THE OBJECT
+			// let w = parseInt(obj.weight);
+			// ADD IT TO THE TOTAL WEIGHT BECAUSE NOTHING CHANGES
+			// let weightGrade = w * (objPercent / 100);
+
+			// ADD THE WEIGHT GRADE (STAYS THE SAME) TO THE TOTAL GRADE
+			// totalGrade += weightGrade;
+
+			newLocalStorage[idx].name = obj.name;
+			newLocalStorage[idx].weight = obj.weight;
+			newLocalStorage[idx].score = obj.score;
+			newLocalStorage[idx].percent = obj.percent;
+		}
+	});
+
+	return newLocalStorage;
+};
+
+const calcGradeBasedOnLocalDeleteObject = (title, e) => {
+	const deleteNewLocalStorage = [];
+	JSON.parse(localStorage.getItem("course_info"))[`${title}`].forEach((obj, idx) => {
+		deleteNewLocalStorage.push({});
+		if (obj.name === e.path[1].querySelectorAll("p")[1].textContent) {
+			let curScore = e.path[1].querySelectorAll("p")[0].textContent;
+			let curScorePts = parseInt(curScore.split("/")[0]);
+			let curScoreTotalPts = parseInt(curScore.split("/")[1]);
+
+			let prevScore = obj.score;
+			let prevScorePts = parseInt(prevScore.split("/")[0]);
+			let prevScoreTotalPts = parseInt(prevScore.split("/")[1]);
+
+			deleteNewLocalStorage[idx].score = `${prevScorePts - curScorePts}/${
+				prevScoreTotalPts - curScoreTotalPts
+			}`;
+
+			deleteNewLocalStorage[idx].percent = `(${(
+				((prevScorePts - curScorePts) / (prevScoreTotalPts - curScoreTotalPts)) *
+				100
+			).toFixed(2)}%)`;
+
+			deleteNewLocalStorage[idx].weight = obj.weight;
+			deleteNewLocalStorage[idx].name = obj.name;
+		} else {
+			deleteNewLocalStorage[idx].weight = obj.weight;
+			deleteNewLocalStorage[idx].name = obj.name;
+			deleteNewLocalStorage[idx].score = obj.score;
+			deleteNewLocalStorage[idx].percent = obj.percent;
+		}
+	});
+
+	return deleteNewLocalStorage;
+};
+
+const main = (iframeContainer) => {
 	localStorage.clear();
 	// CSS FUNCTION TO QUICKLY ADD CSS FOR AN ELEMENT
-	function css(element, style) {
-		for (const property in style) element.style[property] = style[property];
-	}
 	// GET THE IFRAME
-	const iframeContainer = document.getElementById("main-workspace");
 	const arrayOfCategories = []; // the array of categories
 	const localStorageObj = {}; // local storage object for storing the info in local storage
+
+	// const mainDiv = document.createElement("div");
+	// const enableButton = document.createElement("input");
+	// enableButton.type = "checkbox";
+	// enableButton.checked = true;
+	// enableButton.value = "Disable Ez-Campus";
+
+	// mainDiv.appendChild(enableButton);
 
 	// GET TITLE OF THE COURSE
 	const title = iframeContainer.contentWindow.document
@@ -123,12 +191,14 @@ setTimeout(() => {
 	scoreInput.placeholder = "Enter your score here";
 	scoreInput.id = "grade-unique-id";
 
+	// GET THE TOTAL PTS EARNED
 	const firstScoreInput = document.createElement("input");
 	firstScoreInput.type = "number";
 	firstScoreInput.id = "first-score-unique-id";
 	firstScoreInput.placeholder = "Your Score";
 	firstScoreInput.min = 0;
 
+	// GET THE TOTAL POSSIBLE PTS
 	const secondScoreInput = document.createElement("input");
 	secondScoreInput.type = "number";
 	secondScoreInput.id = "second-score-unique-id";
@@ -149,11 +219,12 @@ setTimeout(() => {
 	// APPEND VALS
 	form.appendChild(selectCategories);
 	form.appendChild(nameInput);
-	// form.appendChild(scoreInput);
 	form.appendChild(firstScoreInput);
 	form.appendChild(secondScoreInput);
 	form.appendChild(submit);
+	// mainDiv.appendChild(form);
 
+	// STYLE THE FORM AND ITS COMPONENTS
 	css(form, {
 		width: "100%",
 		height: "4rem",
@@ -163,7 +234,6 @@ setTimeout(() => {
 	});
 	css(selectCategories, { "margin-right": "0.5rem", height: "2rem" });
 	css(nameInput, { "margin-right": "0.5rem", padding: "0.3rem 0.5rem", height: "2rem" });
-	css(scoreInput, { "margin-right": "0.5rem", padding: "0.3rem 0.5rem", height: "2rem" });
 	css(firstScoreInput, { "margin-right": "0.5rem", padding: "0.3rem 0.5rem", height: "2rem" });
 	css(secondScoreInput, { "margin-right": "0.5rem", padding: "0.3rem 0.5rem", height: "2rem" });
 	css(submit, {
@@ -189,6 +259,7 @@ setTimeout(() => {
 	let grade = iframeContainer.contentWindow.document.querySelector(
 		".grading-score__row-spacing"
 	).textContent;
+	// GET THE DEFAULT GRADE FROM INF CAMP
 	grade = grade.substring(1, grade.length - 1);
 	newGradeGrade.textContent = `Calculated Grade: ${grade.slice(1, -1)}`;
 	newGrade.appendChild(newGradeGrade);
@@ -206,16 +277,6 @@ setTimeout(() => {
 		// PREVENT RELOAD
 		event.preventDefault();
 
-		// CHECK IF A VALUE IS A FRACTION OR NOT
-		// if (
-		// 	iframeContainer.contentWindow.document
-		// 		.querySelector("#grade-unique-id")
-		// 		.value.indexOf("/") === -1
-		// ) {
-		// 	console.log("value must be a fraction!");
-		// 	return;
-		// }
-
 		// CREATE NEW ASSIGNMENT
 		const newAssignment = document.createElement("div");
 		// GET ASSIGNMENT NAME VALUE FROM FORM
@@ -224,20 +285,17 @@ setTimeout(() => {
 			iframeContainer.contentWindow.document.querySelector("#name-unique-id").value;
 		// GET ASSIGNMENT SCORE VALUE FROM FORM
 		const score = document.createElement("p");
-		const firstTotalValue = document.createElement("p");
-		const secondTotalValue = document.createElement("p");
-		firstTotalValue.textContent =
+		const firstTotalValue =
 			iframeContainer.contentWindow.document.querySelector("#first-score-unique-id").value;
-		secondTotalValue.textContent =
+		const secondTotalValue =
 			iframeContainer.contentWindow.document.querySelector("#second-score-unique-id").value;
-		score.textContent = `${firstTotalValue.textContent}/${secondTotalValue.textContent}`;
-		// score.textContent =
-		// 	iframeContainer.contentWindow.document.querySelector("#grade-unique-id").value;
-		// GET ASSIGNMENT SCORE VALUE FROM FORM
+		score.textContent = `${firstTotalValue}/${secondTotalValue}`;
+		// GET CATEGORY FROM FORM
 		const category = document.createElement("p");
 		category.textContent =
 			iframeContainer.contentWindow.document.querySelector("#category-unique-id").value;
 
+		// CHECK TO SEE IF USER CHOSE CATEGORY
 		if (category.textContent === "default") {
 			css(errorDiv, {
 				width: "100%",
@@ -250,80 +308,28 @@ setTimeout(() => {
 			return;
 		}
 
+		// IF THEY DIDNT CHOOSE CATEGORY, REMOVE THE ERROR DIV
 		css(errorDiv, { display: "none" });
 		errorMsg.textContent = "";
 
+		// STYLE THE NAME AND SCORE OF THE ASSIGNMENT
 		css(name, { "margin-right": "3rem" });
 		css(score, { "margin-right": "3rem" });
 
-		let newLocalStorage = [];
-		let scoreObj; // the score from the object
-		let weight; // the weight of the category
-		let totalGrade = 0; // the total grade for the class
-		let totalWeight = 0; // the total weight to check because some teacher hide some categories
-		JSON.parse(localStorage.getItem("course_info"))[`${title}`].forEach((obj, idx) => {
-			newLocalStorage.push({});
-			// IF THE CATEGORY IS THE OBJECT NAME --> THE ASSIGNMENT IS ADDED TO THAT CATEGORY
-			if (obj.name === category.textContent) {
-				// SPLIT THE SCORE PROPERTY FROM THE MAIN OBJECT
-				scoreObj = obj.score;
-				scoreObj = scoreObj.split("/");
-
-				// GET THE WEIGHT PROPERTY FROM THE MAIN OBJECT
-				weight = parseInt(obj.weight);
-				totalWeight += weight;
-
-				// GET TOTAL POSSIBLE POINTS AND TOTAL POINTS EARNED TO DIVIDE THEM
-				let totalPtsPossible =
-					parseInt(scoreObj[1]) + parseInt(secondTotalValue.textContent);
-				let totalPtsEarned = parseInt(scoreObj[0]) + parseInt(firstTotalValue.textContent);
-
-				newLocalStorage[idx].score = `${totalPtsEarned}/${totalPtsPossible}`;
-
-				// CALCULATE THE PERCENT OF THE WEIGHTAGE
-				let weightGrade = weight * (totalPtsEarned / totalPtsPossible / 100);
-				newLocalStorage[idx].percent = `(${(
-					(totalPtsEarned / totalPtsPossible) *
-					100
-				).toFixed(2)}%)`;
-
-				newLocalStorage[idx].name = obj.name;
-				newLocalStorage[idx].weight = obj.weight;
-
-				// ADD THE WEIGHT GRADE TO THE TOTAL GRADE
-				// totalGrade += weightGrade;
-			} else {
-				// IF THE CATEGORY THAT IS ADDED IS NOT THE OBJECT
-				// GET PERCENT OF THE OBJECT
-				let objPercent = obj.percent.slice(1);
-				objPercent = objPercent.slice(0, -2);
-				objPercent = parseFloat(objPercent);
-
-				// WEIGHT OF THE OBJECT
-				let w = parseInt(obj.weight);
-				// ADD IT TO THE TOTAL WEIGHT BECAUSE NOTHING CHANGES
-				totalWeight += w;
-				let weightGrade = w * (objPercent / 100);
-
-				// ADD THE WEIGHT GRADE (STAYS THE SAME) TO THE TOTAL GRADE
-				// totalGrade += weightGrade;
-
-				newLocalStorage[idx].name = obj.name;
-				newLocalStorage[idx].weight = obj.weight;
-				newLocalStorage[idx].score = obj.score;
-				newLocalStorage[idx].percent = obj.percent;
-			}
-		});
-		// IF TEACHER HAS NOT ADDED A CATEGOY, THEN IT IS BBY DEFAULT 100%, SO JUST ADD THE REMAINING VALUE
-		totalGrade = 0;
-		newLocalStorage.forEach(({ weight, percent }, idx) => {
-			totalGrade += parseInt(weight) * parseFloat(parseFloat(percent.slice(1, -2)));
-		});
-		totalGrade = totalGrade / totalWeight;
-		newGradeGrade.textContent = `Calculated Grade: ${totalGrade.toFixed(2)}%`;
+		// WHEN FORM SUBMITS EDIT GRADE
+		const formSubmitLocalObj = calcGradeBasedOnLocalObject(
+			title,
+			category,
+			firstTotalValue,
+			secondTotalValue
+		);
+		let formSubmitTotalGrade = calcTotalGrade(formSubmitLocalObj);
+		newGradeGrade.textContent = `Calculated Grade: ${formSubmitTotalGrade.toFixed(2)}%`;
 		let t = {};
-		t[`${title}`] = newLocalStorage;
+		t[`${title}`] = formSubmitLocalObj;
 		localStorage.setItem("course_info", JSON.stringify(t));
+
+		console.log(score.textContent);
 
 		// ADD THE NEW ASSIGNMENT TO THE DOM
 		const deleteButton = document.createElement("button");
@@ -335,74 +341,95 @@ setTimeout(() => {
 		newAssignment.appendChild(deleteButton);
 
 		newAssignment.id = `${name.textContent.split().join("-")}`;
+		css(newAssignment, { display: "flex" });
 
 		// CHANGE THE CONTENT FROM NO ASSIGNMENTS TO ADDED ASSIGNMENTS
 		assignmentHeader.textContent = "Added Assignments:";
 		addedAssignments.appendChild(newAssignment);
 
-		css(newAssignment, { display: "flex" });
-
 		deleteButton.addEventListener("click", (e) => {
-			const deleteNewLocalStorage = [];
-			JSON.parse(localStorage.getItem("course_info"))[`${title}`].forEach((obj, idx) => {
-				deleteNewLocalStorage.push({});
-				if (obj.name === e.path[1].querySelectorAll("p")[1].textContent) {
-					let curScore = e.path[1].querySelectorAll("p")[0].textContent;
-					let curScorePts = curScore.split("/")[0];
-					let curScoreTotalPts = curScore.split("/")[1];
-
-					let prevScore = obj.score;
-					let prevScorePts = prevScore.split("/")[0];
-					let prevScoreTotalPts = prevScore.split("/")[1];
-
-					deleteNewLocalStorage[idx].score = `${prevScorePts - curScorePts}/${
-						prevScoreTotalPts - curScoreTotalPts
-					}`;
-
-					deleteNewLocalStorage[idx].percent = `(${(
-						((prevScorePts - curScorePts) / (prevScoreTotalPts - curScoreTotalPts)) *
-						100
-					).toFixed(2)}%)`;
-
-					deleteNewLocalStorage[idx].weight = obj.weight;
-					deleteNewLocalStorage[idx].name = obj.name;
-				} else {
-					deleteNewLocalStorage[idx].weight = obj.weight;
-					deleteNewLocalStorage[idx].name = obj.name;
-					deleteNewLocalStorage[idx].score = obj.score;
-					deleteNewLocalStorage[idx].percent = obj.percent;
-				}
-			});
-
-			let deleteNewTotalGrade = 0;
-			let deleteNewTotalWeightage = 0;
-			deleteNewLocalStorage.forEach(({ weight, percent }, idx) => {
-				deleteNewTotalGrade +=
-					parseInt(weight) * parseFloat(parseFloat(percent.slice(1, -2)));
-				deleteNewTotalWeightage += parseInt(weight);
-			});
-			deleteNewTotalGrade = deleteNewTotalGrade / deleteNewTotalWeightage;
-			newGradeGrade.textContent = `Calculated Grade: ${deleteNewTotalGrade.toFixed(2)}%`;
+			// let deleteNewTotalGrade = 0;
+			// let deleteNewTotalWeightage = 0;
+			// deleteNewLocalStorage.forEach(({ weight, percent }, idx) => {
+			// 	deleteNewTotalGrade +=
+			// 		parseInt(weight) * parseFloat(parseFloat(percent.slice(1, -2)));
+			// 	deleteNewTotalWeightage += parseInt(weight);
+			// });
+			// deleteNewTotalGrade = deleteNewTotalGrade / deleteNewTotalWeightage;
+			const deleteNewLocalStorage = calcGradeBasedOnLocalDeleteObject(title, e);
+			const deleteTotalGrade = calcTotalGrade(deleteNewLocalStorage);
+			newGradeGrade.textContent = `Calculated Grade: ${deleteTotalGrade.toFixed(2)}%`;
 			let dt = {};
 			dt[`${title}`] = deleteNewLocalStorage;
 			localStorage.setItem("course_info", JSON.stringify(dt));
 
-			// const score = obj.score;
-			// const pts = e.path[1].querySelector("p")[0].textContent.split("/")[0];
-			// const totalPts = e.path[1].querySelector("p")[0].textContent.split("/")[1];
-
-			// obj.percent = `(${
-			// 	(parseInt(score.split("/")[0]) - parseInt(pts)) /
-			// 	(parseInt(score.split("/")[1]) - parseInt(totalPts))
-			// }%)`;
-
-			// const scorePts = parseInt(score.split("/")[0]) - parseInt(pts);
-			// const scoreTotalPts = parseInt(score.split("/")[1]) - parseInt(totalPts);
-
-			// console.log(e);
 			e.path[1].parentNode.removeChild(e.path[1]);
 			if (addedAssignments.querySelectorAll("div").length === 1)
 				assignmentHeader.textContent = "No assignments added.";
 		});
 	});
-}, 10000);
+};
+
+setTimeout(async () => {
+	const iframeContainer = document.getElementById("main-workspace");
+	function waitForElm(item) {
+		return new Promise((resolve) => {
+			if (item) {
+				return resolve(item);
+			}
+
+			const observer = new MutationObserver((mutations) => {
+				if (item) {
+					resolve(item);
+					observer.disconnect();
+				}
+			});
+
+			observer.observe(document.body, {
+				childList: true,
+				subtree: true,
+			});
+		});
+	}
+
+	const elm = await waitForElm(iframeContainer);
+
+	const title = elm.contentWindow.document.querySelector(".card__header.sticky.z-10");
+	if (!title) {
+		const titleInterval = setInterval(() => {
+			if (elm.contentWindow.document.querySelector(".card__header.sticky.z-10")) {
+				if (
+					!elm.contentWindow.document
+						.querySelectorAll("button.divider__header > div")[0]
+						.querySelector("div")
+				) {
+					const workspaceDiv =
+						elm.contentWindow.document.querySelector(".workspace-content");
+					const div = document.createElement("div");
+					const header = document.createElement("h2");
+					header.textContent =
+						"Your Teacher has not chosen to add weights on your class, please read more here";
+					div.appendChild(header);
+					css(div, {
+						width: "100%",
+						height: "4rem",
+						display: "flex",
+						"justify-content": "center",
+						"align-items": "center",
+					});
+					workspaceDiv.prepend(div);
+					clearInterval(titleInterval);
+					return;
+				}
+				console.log(
+					elm.contentWindow.document.querySelector("button.divider__header > div > div")
+				);
+				clearInterval(titleInterval);
+				main(elm);
+			}
+		}, 1000);
+	}
+
+	// setTimeout(main, 10000);
+	// main(elm);
+}, 1000);
